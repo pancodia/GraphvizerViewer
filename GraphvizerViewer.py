9542c9b74087ce3b32d3b4c2cfb5c3ee087a7dd9
Hao Lee <haolee.swjtu@gmail.com>
2020-01-12 15:09:53 +0800

    Completely Refactor
    inherit QGraphicsView
    
    Signed-off-by: Hao Lee <haolee.swjtu@gmail.com>

--- GraphvizerViewer.py
+++ GraphvizerViewer.py
@@ -1,71 +1,53 @@
 import sys
 from PySide2.QtWidgets import QApplication, QGraphicsView, QGraphicsScene
-from PySide2.QtGui import QImage, QPixmap, QMouseEvent, QWheelEvent
+from PySide2.QtGui import QImage, QPixmap, QMouseEvent, QWheelEvent, QPainterPath
 from PySide2.QtCore import Signal, Slot, QObject, QEvent, QPointF, Qt
+import time
 
-class GraphvizerViewer(QObject):
-	mouse_middle_button_wheeled = Signal(QWheelEvent)
-	mouse_left_button_pressed = Signal(QMouseEvent)
-	mouse_right_button_pressed = Signal(QMouseEvent)
-	mouse_left_button_released = Signal(QMouseEvent)
-	mouse_right_button_released = Signal(QMouseEvent)
-	mouse_moved = Signal(QMouseEvent)
-
+class GraphvizerViewer(QGraphicsView):
 	def __init__(self):
 		super(GraphvizerViewer, self).__init__(None)
-		self.view = QGraphicsView()
 		self.scene = QGraphicsScene()
-		self.view.setScene(self.scene)
+		self.setScene(self.scene)
 		self.image = QImage("E:\\multimedia\\picture\\忍者神龟\\F200705151517471444526594.jpg")
 		self.scene.addPixmap(QPixmap.fromImage(self.image))
-		self.view.show()
-		self.mouse_middle_button_wheeled.connect(self.zoom)
-		self.mouse_left_button_pressed.connect(self.pan)
-		self.mouse_left_button_released.connect(self.pan_done)
+		self.last_release_time = 0
 
-	@Slot(QWheelEvent)
-	def zoom(self, wheel_event):
-		# https://doc.qt.io/qtforpython/PySide2/QtGui/QWheelEvent.html#PySide2.QtGui.PySide2.QtGui.QWheelEvent.angleDelta
+	def mousePressEvent(self, mouse_event): # QMouseEvent
+		if mouse_event.button() == Qt.LeftButton:
+			self.setDragMode(QGraphicsView.ScrollHandDrag)
+		elif mouse_event.button() == Qt.RightButton:
+			self.setDragMode(QGraphicsView.RubberBandDrag)
+		QGraphicsView.mousePressEvent(self, mouse_event)
+
+	def mouseReleaseEvent(self, mouse_event): # QMouseEvent
+		QGraphicsView.mouseReleaseEvent(self, mouse_event)
+		if mouse_event.button() == Qt.LeftButton:
+			self.setDragMode(QGraphicsView.NoDrag)
+		elif mouse_event.button() == Qt.RightButton:
+			self.setDragMode(QGraphicsView.NoDrag)
+
+			now = time.time()
+			delta = now - self.last_release_time
+			self.last_release_time = now
+			if delta < 0.3: # fast double click
+				self.resetTransform() # Reset to original size
+				return
+			# Maybe a selection
+			selection = self.scene.selectionArea().boundingRect()
+			self.scene.setSelectionArea(QPainterPath())
+			if selection.isValid():
+				self.fitInView(selection, Qt.KeepAspectRatio)
+
+	def wheelEvent(self, wheel_event): # QWheelEvent
 		num_degrees = wheel_event.angleDelta().y() / 8
 		num_steps = num_degrees / 15
 		coefficient = 1 + (num_steps * 0.25)
-		self.view.scale(coefficient, coefficient)
-
-	@Slot(QMouseEvent)
-	def pan(self, mouse_event):
-		self.view.setDragMode(QGraphicsView.ScrollHandDrag)
-
-	@Slot(QMouseEvent)
-	def pan_done(self, mouse_event):
-		self.view.setDragMode(QGraphicsView.NoDrag)
-
-	# https://git.io/JepEs
-	# https://www.qtcentre.org/threads/18447-wheelEvent-not-working-in-an-eventFilter
-	def eventFilter(self, obj, event):
-		if event.type() == QEvent.Wheel:
-			self.mouse_middle_button_wheeled.emit(event)
-			return True
-		elif event.type() == QEvent.MouseButtonPress:
-			if event.button() == Qt.LeftButton:
-				self.mouse_left_button_pressed.emit(event)
-			elif event.button() == Qt.RightButton:
-				self.mouse_right_button_pressed.emit(event)
-			elif event.button() == Qt.MidButton:
-				pass
-			return False
-		elif event.type() == QEvent.MouseButtonRelease:
-			if event.button() == Qt.LeftButton:
-				self.mouse_left_button_released.emit(event)
-			elif event.button() == Qt.RightButton:
-				self.mouse_right_button_released.emit(event)
-			elif event.button() == Qt.MidButton:
-				pass
-			return False
-		else:
-			return QObject.eventFilter(self, obj, event)
+		self.scale(coefficient, coefficient)
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
 	viewer = GraphvizerViewer()
-	app.installEventFilter(viewer)
+	viewer.setWindowTitle("Graphvizer Viewer")
+	viewer.show()
 	sys.exit(app.exec_())
